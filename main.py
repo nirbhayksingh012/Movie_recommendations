00316d004690e3d42f92c92d2a0a1214d5bb8cec
nirbhay singh <nirbhayksingh14@gmail.com>
2026-08-02 23:20:21 +0530

    comeeee

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = FastAPI(title="Movie Recommender API", version="3.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for local streamlit
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
